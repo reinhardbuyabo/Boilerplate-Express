8c477b0d30c177abd2510854b0d8fa45a7d819f4
Replit user <>
2023-02-04 12:16:48 +0000

    feat: chain middleware to create a time server

--- myApp.js
+++ myApp.js
@@ -26,6 +26,15 @@ app.get("/json", function(req, res){
   }
 })
 
+app.get("/now", function(req, res, next){
+  req.time = new Date().toString()
+  next()
+}, function(req, res){
+  res.send({
+    time: req.time
+  })
+})
+
 
 
 
