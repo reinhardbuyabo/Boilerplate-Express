aae91db21ab05199856286950d93cbbb460803c5
Replit user <>
2023-02-04 08:17:07 +0000

    chore: Using .env

--- myApp.js
+++ myApp.js
@@ -1,15 +1,25 @@
 let express = require('express');
 let app = express();
+//const mySecret = process.env['MESSAGE_STYLE']
 
 console.log("Hello World")
-
 app.get("/", function(req, res){
   res.sendFile(__dirname + '/views/index.html')
 })
 
 app.use("/public", express.static(__dirname + "/public"))
 
-
+app.get("/json", function(req, res){
+  if (process.env['MESSAGE_STYLE'] === "uppercase") {
+    res.json({
+      "message": "HELLO JSON"
+    })
+  } else {
+    res.json({
+      "message": "Hello json"
+    })
+  }
+})
 
 
 
